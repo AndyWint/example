861ff9bbefd67db9fcea00e86f6e7278689e3020
[AndyWinter] <avake3d@yandex.ru>
2023-02-13 13:26:24 +0500

    Добавил программу поиска большего числа

---
+++ Example02/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberA = 5;
+int numberB = 7;
+
+Console.Write("Большее число, ");
+    if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+    else 
+{
+    Console.WriteLine(numberB);
+} 
+
+Console.Write("Меньшее число, ");
+    if (numberA < numberB)
+{
+    Console.WriteLine(numberA);
+}
+    else 
+{
+    Console.WriteLine(numberB);
+}
